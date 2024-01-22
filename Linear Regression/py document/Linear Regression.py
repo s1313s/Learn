b715a185531f9232d9e2903ae9a34597e4185ea9
Xia <xiassss2@gmail.com>
2024-01-23 07:21:02 +0800

    Q

--- Linear Regression/py document/Linear Regression.py
+++ Linear Regression/py document/Linear Regression.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-# 从文本文件中读取数据，并返回 NumPy 数组
+# Define The Load Data Functions
 def read_txt_file(file_path):
     data = np.loadtxt(file_path)  
     x_data, y_data = data[:, 0], data[:, 1]
     return x_data, y_data
 
-# 定义画图函数
+# Define The Drawing Function
 def plot_fit_sklearn(x, y, model):
     fit_line = model.predict(x.reshape(-1, 1))
     plt.plot(x, fit_line, 'g', label='Linear Fit')
@@ -20,7 +20,7 @@ def plot_fit_sklearn(x, y, model):
     plt.title('Best fit to our data (sklearn)')
     plt.show()
 
- # 定义计算残差函数
+ # Define The Computed Residual Function
 def evaluate_residuals_sklearn(x, y, model):
     fit_line = model.predict(x.reshape(-1, 1))
     residuals = y - fit_line
@@ -32,16 +32,16 @@ def evaluate_residuals_sklearn(x, y, model):
     print(f"Mean of Residuals: {mean_residuals:.8f}")
     print(f"Standard Deviation of Residuals: {std_residuals:.4f}")
 
-# 读取数据
+# Reading Data
 x_data, y_data = read_txt_file('./Linear Regression/dataset/data 1.txt')
 
-# 使用 scikit-learn 进行线性拟合
+# Linear Fitting Using scikit-learn
 x_data_2d = x_data.reshape(-1, 1)
 model = LinearRegression().fit(x_data_2d, y_data)
 
-# 计算残差
+# Calculated Residual
 evaluate_residuals_sklearn(x_data, y_data, model)
 
-# 绘制数据和拟合曲线
+# Plot Data And Fit Curves
 plot_fit_sklearn(x_data, y_data, model)
 
